fa63ad043067c30aa9caa07aabd5b1c76588417d
joshbj360 <joshuaakibu@gmail.com>
2022-08-24 13:37:42 +0100

    Building Components

---
+++ src/components/TaskList.stories.js
@@ -0,0 +1,65 @@
+import TaskList from './TaskList.vue'
+import * as TaskStories from './Task.stories'
+
+export default {
+    component: "TaskList",
+    decorators: [() => ({template: '<div style="margin: 3em;"><story/></div>'})],
+    argTypes: {
+        onPinTask: {},
+        onArchiveTask: {},
+    }
+}
+
+const Template = args => ({
+    components: {TaskList},
+    setup() {
+        return {args, ...TaskStories.actionsData}
+    },
+    template: '<TaskList v-bind="args"  />'
+})
+
+export const Default = Template.bind({})
+Default.args = {
+    /* 
+    Shaping the stories through the args composition
+    The data was inherited form the Default story in Task.stories.js.
+    */
+   tasks: [
+    {...TaskStories.Default.args.tasks, id: 1, title: 'Task 1'},
+    {...TaskStories.Default.args.tasks, id: 2, title: 'Task 2'},
+    {...TaskStories.Default.args.tasks, id: 3, title: 'Task 3'},
+    {...TaskStories.Default.args.tasks, id: 4, title: 'Task 4'},
+    {...TaskStories.Default.args.tasks, id: 5, title: 'Task 5'},
+    {...TaskStories.Default.args.tasks, id: 6, title: 'Task 6'},
+   ],
+}
+
+export const WithPinnedTask = Template.bind({})
+WithPinnedTask.args = {
+    tasks: [
+        ...Default.args.tasks.slice(0, 5),
+        {id: 6, title: 'Task 6 (pinned)', state: 'TASK_PINNED'}
+    ]
+
+}
+export const WithArchivedTask = Template.bind({})
+WithArchivedTask.args = {
+    tasks: [
+        ...Default.args.tasks.slice(0, 3),
+        {id: 4, title: 'Wash clothes', state: 'TASK_ARCHIVED'},
+        {id: 5,  title: 'cooking',state: 'TASK_ARCHIVED'},
+        {id: 6, title: 'Go to church', state: 'TASK_ARCHIVED'},
+    ]
+}
+
+export const Loading = Template.bind({})
+Loading.args = {
+    tasks: [],
+    loading: true
+}
+
+export const Empty = Template.bind({})
+Empty.args = { 
+    ...Loading.args,
+    loading: false,
+}
---
+++ src/components/TaskList.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="list-items">
+        <template v-if="loading">
+            <div v-for="t in 6" :key="t" class="loading-item">
+                <span class="glow-checkbox" />
+                <span class="glow-text">
+                    <span>Loading...</span><span>cool</span><span>state</span>
+                </span>
+            </div>
+        </template>
+        <template v-else-if="isEmpty">
+            <div class="wrapper-message">
+                <span class="icon-check" />
+                <p class="title-message">You have no task.</p>
+                <p class="subtitle-message">Sit back and relax.</p>
+            </div>
+        </template>
+        <template v-else>
+            <Task
+                v-for="task in tasksInOrder"
+                :key="task.id"
+                :task="task"
+                @archive-task="onArchiveTask"
+                @pin-task="onPinTask"
+            ></Task>
+        </template>
+    </div>
+</template>
+
+<script>
+    import { emit } from 'process';
+import { computed, reactive } from 'vue';
+import Task from './Task.vue'
+    export default {
+        name: "TaskList",
+        components: { Task },
+        props: { 
+            tasks:{type: Array, required: true, default: () => []},
+            loading: { type: Boolean, default: false }
+        },
+
+        emits: ['archive-task', 'pin-task'],
+
+        setup(props, {emits}) {
+            props = reactive(props)
+            
+            return {
+                isEmpty: computed(() => props.tasks.length === 0),
+                tasksInOrder: computed(() => {
+                    return [
+                        ...props.tasks.filter(t => t.state === "TASK_PINNED"),
+                        ...props.tasks.filter(t => t.state !== "TASK_PINNED"),
+                    ]
+                }),
+
+                /* 
+                Event handler for OnArchiveTask
+                */
+                onArchiveTask(taskId) {
+                    emit('archive-task', taskId)
+                },
+                /* 
+                Event handler for OnPinTask
+                */
+                onPinTask(taskId) {
+                    emit('pin-task', taskId)
+                }
+            }
+        }
+}
+</script>
