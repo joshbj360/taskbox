9dc05e803d9b5ac0687295d606ff8aa1f788b714
joshbj360 <joshuaakibu@gmail.com>
2022-08-25 22:49:05 +0100

    add tasklist

---
+++ src/components/TaskList.vue
@@ -0,0 +1,31 @@
+<template>
+    <PureTaskList :tasks="tasks" @archive-task=archiveTask @pin-task="pinTask"/>
+</template>
+
+<script>
+    import PureTaskList from './PureTaskList.vue'
+
+    import { computed } from 'vue'
+    import { useStore } from 'vuex'
+import PureTaskList from './PureTaskList.vue'
+
+    export default {
+    component: PureTaskList,
+    name: "TaskList",
+    setup() {
+        // Creates a store instance
+        const store = useStore();
+        // Retrieves the tasks from the store instance
+        const tasks = computed(() => store.state.tasks);
+        // Dispatches the action back to the store
+        const archiveTask = task => store.dispatch("archiveTask", task);
+        const pinTask = task => store.dispatch("pinTask", task);
+        return {
+            tasks,
+            archiveTask,
+            pinTask,
+        };
+    },
+    components: { PureTaskList }
+}
+</script>
